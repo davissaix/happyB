2198f8b52fb1e6dafadfbfd8cb38a734f43f5428
David Avila <davissaixgmail.com>
2022-08-08 03:22:37 -0500

    coloresSin

--- src/script.js
+++ src/script.js
@@ -9,7 +9,7 @@ import { AxesHelper } from 'three'
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+// const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
